0c824a706ab1521ed8d5972df216e11b51ccb4f6
Max Rytik <max777dan@gmail.com>
2024-02-18 13:35:41 +0300

    PropTypes
    проверка пропсов по типу

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -109,6 +110,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired //с помощью PropTypes проверяем является ли функцией и существует пропс onCharSelected
 }
 
 export default CharList;
